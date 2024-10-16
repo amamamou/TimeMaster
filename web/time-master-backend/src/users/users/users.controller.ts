eb6b9b5e3f4158cd0eb83fd6d1f1c311156404f4
amamamou <amamoumaram88@gmail.com>
2024-10-16 01:11:23 +0100

    Login

--- web/time-master-backend/src/users/users/users.controller.ts
+++ web/time-master-backend/src/users/users/users.controller.ts
@@ -1,23 +1,25 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Post, Req, Res } from '@nestjs/common';
-import { Request, Response } from 'express';
-import { UsersService } from './users.service'; 
+import { Controller, Post, Body, Session, Res } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Response } from 'express';
 
-@Controller('users') // /users
+@Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post('login') // /users/login
-  async login(@Req() req: Request, @Res() res: Response) {
-    const { username, password } = req.body;
-
+  @Post('login')
+  async login(
+    @Body('username') username: string,
+    @Body('password') password: string,
+    @Session() session: Record<string, any>,
+    @Res() res: Response,
+  ) {
     const user = await this.usersService.validateUser(username, password);
-
     if (user) {
-      req.session.user = user; 
-      return res.status(200).json({ message: 'Login successful', user });
+      session.userId = user.id; // Save user info in session
+      return res.status(200).json({ message: 'Login successful', redirectUrl: '/' });
     } else {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: 'Incorrect username or password' });
     }
   }
 }
--- web/time-master-frontend/src/app/app.module.ts
+++ web/time-master-frontend/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './user/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, // Ensure this is present
+    HttpClientModule // Add HttpClientModule here
+
     ],
   providers: [],
   bootstrap: [AppComponent]
