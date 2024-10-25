d6933f588a8aacb3c521a77929ca423108cd0f98
sarrah783 <alloussarra0@gmail.com>
2024-10-25 23:07:00 +0100

    Delete web directory

--- web/time-master-backend/src/express-session.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable prettier/prettier */
-import 'express-session';
-
-declare module 'express-session' {
-  interface SessionData {
-    user: any; // You can replace 'any' with a more specific type if you have one for the user
-  }
-}
